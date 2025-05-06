21fe8bfe764114f05ba2333e04114c28a5a4bcac
Andrei Mateaș <mateasandrei10@gmail.com>
2025-05-06 21:10:16 +0300

    1

--- src/main/java/com/siemens/internship/ItemService.java
+++ src/main/java/com/siemens/internship/ItemService.java
@@ -69,7 +69,7 @@ public class ItemService {
                     }
 
                     processedCount++;
-
+//
                     item.setStatus("PROCESSED");
                     itemRepository.save(item);
                     processedItems.add(item);
